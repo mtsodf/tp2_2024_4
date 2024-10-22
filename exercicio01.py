0048c7c31717a428ae5442f02fc1fc6fc7646645
Mateus Oliveira <mtsodf@gmail.com>
2024-10-21 22:49:28 -0300

    exercicio01.py

--- exercicio01.py
+++ exercicio01.py
@@ -1,1 +1,25 @@
 # Exercício 01
+
+"""
+Em Python, funções são blocos de código que realizam uma tarefa específica e podem ser reutilizadas em diferentes partes do programa. Sobre as funções em Python, responda às perguntas abaixo:
+"""
+
+# 1) O que é uma função e qual a principal vantagem de utilizá-las em um programa?
+"""
+Escreva sua resposta
+"""
+
+# 2) Qual é o número mínimo e o número máximo de parâmetros que uma função pode ter em Python?
+"""
+Escreva sua resposta
+"""
+
+# 3) O que é um parâmetro padrão (default) em uma função? Escreva um exemplo de função com um parâmetro padrão e explique seu funcionamento.
+"""
+Escreva sua resposta
+Modifique o nome e escreva a função abaixo
+"""
+
+
+def sua_funcao():
+    pass
